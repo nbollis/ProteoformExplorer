6329ff70deb0443a35599cc919b20fe90b085306
ROBERT MILLIKIN <rmillikin@wisc.edu>
2021-06-18 12:50:45 -0500

    easier pfm family visualization, nodes+edges

---
+++ GUI/GraphVisualization/Node.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using ScottPlot;
+using ScottPlot.Plottable;
+
+namespace GUI
+{
+    public class Node
+    {
+        public double X { get; private set; }
+        public double Y { get; private set; }
+        private List<Edge> _edges;
+        public ScatterPlot VisualRepresentation { get; set; }
+        public Text TextAnnotation;
+
+        public Node(double x, double y, string textAnnotation = null)
+        {
+            this.X = x;
+            this.Y = y;
+            _edges = new List<Edge>();
+
+            VisualRepresentation = new ScatterPlot(new double[] { x }, new double[] { y });
+            VisualRepresentation.LineWidth = 0;
+            VisualRepresentation.MarkerSize = 70;
+
+            TextAnnotation = new Text();
+            TextAnnotation.X = X;
+            TextAnnotation.Y = Y;
+            TextAnnotation.Label = textAnnotation;
+        }
+
+        public void AddConnection(Node connectedNode, string edgeAnnotation, bool bidirectional = true)
+        {
+            var edge = new Edge(this, connectedNode, edgeAnnotation);
+
+            this._edges.Add(edge);
+
+            if (bidirectional)
+            {
+                connectedNode.AddConnection(this, edgeAnnotation, false);
+            }
+        }
+
+        public bool RemoveConnection(Node connectedNode)
+        {
+            var edgeToRemove = Edges.FirstOrDefault(p => p.ConnectedNodes.Contains(connectedNode));
+
+            if (edgeToRemove != null)
+            {
+                _edges.Remove(edgeToRemove);
+                return true;
+            }
+
+            return false;
+        }
+
+        public IEnumerable<Edge> Edges
+        {
+            get
+            {
+                return _edges;
+            }
+        }
+
+        public IEnumerable<Node> ConnectedNodes()
+        {
+            foreach (var edge in Edges)
+            {
+                foreach (var node in edge.ConnectedNodes)
+                {
+                    if (this != node)
+                    {
+                        yield return node;
+                    }
+                }
+            }
+        }
+    }
+}
--- GUI/Pages/ProteoformFamilyVisualization.xaml.cs
+++ GUI/Pages/ProteoformFamilyVisualization.xaml.cs
@@ -20,16 +20,17 @@ namespace GUI.Pages
         {
             InitializeComponent();
 
-            var exampleFamily = new List<VisualizedProteoformFamilyMember>();
+            var exampleFamily = new List<VisualizedProteoformFamilyMember> 
+            {
+                new VisualizedProteoformFamilyMember(VisualizedType.Gene, @"TestGene", 10, 0),
+                new VisualizedProteoformFamilyMember(VisualizedType.TheoreticalProteoform, @"Unmodified", 15, 5),
+                new VisualizedProteoformFamilyMember(VisualizedType.TopDownExperimentalProteoform, @"Unmodified", 15, 10),
+                new VisualizedProteoformFamilyMember(VisualizedType.IntactMassExperimentalProteoform, @"Acetyl", 5, 5),
+            };
 
-            exampleFamily.Add(new VisualizedProteoformFamilyMember(VisualizedType.Gene, @"TestGene", 10, 0));
-            exampleFamily.Add(new VisualizedProteoformFamilyMember(VisualizedType.TheoreticalProteoform, @"Unmodified", 15, 5));
-            exampleFamily.Add(new VisualizedProteoformFamilyMember(VisualizedType.TopDownExperimentalProteoform, @"Unmodified", 15, 10));
-            exampleFamily.Add(new VisualizedProteoformFamilyMember(VisualizedType.IntactMassExperimentalProteoform, @"Acetyl", 5, 5));
-
-            exampleFamily[0].Connections.Add(("0 Da", exampleFamily[1]));
-            exampleFamily[0].Connections.Add(("0 Da", exampleFamily[2]));
-            exampleFamily[1].Connections.Add(("42 Da", exampleFamily[3]));
+            exampleFamily[0].Node.AddConnection(exampleFamily[1].Node, "0 Da");
+            exampleFamily[0].Node.AddConnection(exampleFamily[2].Node, "0 Da");
+            exampleFamily[1].Node.AddConnection(exampleFamily[3].Node, "42 Da");
 
             DrawProteoformFamily(exampleFamily);
         }
@@ -40,67 +41,61 @@ namespace GUI.Pages
 
             foreach (var proteoform in proteoformFamily)
             {
-                foreach (var connection in proteoform.Connections)
+                foreach (var connection in proteoform.Node.Edges)
                 {
-                    pfmFamilyVisualizationChart.Plot.AddLine(proteoform.x, proteoform.y, connection.Item2.x, connection.Item2.y, Color.Black, lineWidth: 1);
-                    pfmFamilyVisualizationChart.Plot.AddText(connection.edgeAnnotation, (proteoform.x + connection.Item2.x) / 2.0, (proteoform.y + connection.Item2.y) / 2.0, 12, Color.Black);
+                    connection.VisualRepresentation.LineWidth = 1;
+                    connection.VisualRepresentation.Color = Color.Black;
+                    connection.TextAnnotation.Color = Color.Black;
+
+                    pfmFamilyVisualizationChart.Plot.Add(connection.VisualRepresentation);
+                    pfmFamilyVisualizationChart.Plot.Add(connection.TextAnnotation);
                 }
             }
 
             foreach (var proteoform in proteoformFamily)
             {
-                pfmFamilyVisualizationChart.Plot.Add(proteoform.Graphic);
-                pfmFamilyVisualizationChart.Plot.AddText(proteoform.NodeAnnotation, proteoform.x, proteoform.y, 12, Color.Black);
+                pfmFamilyVisualizationChart.Plot.Add(proteoform.Node.VisualRepresentation);
+                pfmFamilyVisualizationChart.Plot.Add(proteoform.Node.TextAnnotation);
             }
         }
     }
 
     public class VisualizedProteoformFamilyMember
     {
-        public readonly string NodeAnnotation;
-        public IPlottable Graphic { get; private set; }
-        public List<(string edgeAnnotation, VisualizedProteoformFamilyMember)> Connections { get; set; }
         public VisualizedType Type { get; private set; }
-        public double x;
-        public double y;
+        public Node Node { get; private set; }
 
         public VisualizedProteoformFamilyMember(VisualizedType type, string nodeAnnotation, double x, double y)
         {
             Type = type;
-            NodeAnnotation = nodeAnnotation;
-            this.x = x;
-            this.y = y;
-            Connections = new List<(string edgeAnnotation, VisualizedProteoformFamilyMember)>();
+            Node = new Node(x, y, nodeAnnotation);
             BuildProteoformVisualRepresentation();
         }
 
         private void BuildProteoformVisualRepresentation()
         {
-            ScatterPlot temp = new ScatterPlot(new double[] { x }, new double[] { y });
-            temp.MarkerSize = 70;
-            temp.LineWidth = 0;
-            Graphic = temp;
+            Node.VisualRepresentation.MarkerSize = 70;
 
             switch (Type)
             {
                 case VisualizedType.Gene:
-                    temp.MarkerShape = ScottPlot.MarkerShape.filledDiamond;
-                    temp.Color = Color.DeepPink;
+                    Node.VisualRepresentation.MarkerShape = ScottPlot.MarkerShape.filledDiamond;
+                    Node.VisualRepresentation.Color = Color.DeepPink;
                     break;
 
                 case VisualizedType.TheoreticalProteoform:
-                    temp.MarkerShape = ScottPlot.MarkerShape.filledCircle;
-                    temp.Color = Color.LimeGreen;
+                    Node.VisualRepresentation.MarkerShape = ScottPlot.MarkerShape.filledCircle;
+                    Node.VisualRepresentation.Color = Color.LimeGreen;
                     break;
 
                 case VisualizedType.TopDownExperimentalProteoform:
-                    temp.MarkerShape = ScottPlot.MarkerShape.filledCircle;
-                    temp.Color = Color.SlateBlue;
+                    Node.VisualRepresentation.MarkerShape = ScottPlot.MarkerShape.filledCircle;
+                    Node.VisualRepresentation.Color = Color.SlateBlue;
                     break;
 
                 case VisualizedType.IntactMassExperimentalProteoform:
-                    temp.MarkerShape = ScottPlot.MarkerShape.filledCircle;
-                    temp.Color = Color.DodgerBlue;
+                    Node.VisualRepresentation.MarkerShape = ScottPlot.MarkerShape.filledCircle;
+                    Node.VisualRepresentation.Color = Color.DodgerBlue;
                     break;
             }
         }
